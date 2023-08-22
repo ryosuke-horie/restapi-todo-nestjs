8d5fbe5d6cd18ba991013e4c75e5ee6cd1285281
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-22 10:38:14 +0900

    prismaサービスを定義し各Moduleでimport

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [TodoController],
   providers: [TodoService],
 })
