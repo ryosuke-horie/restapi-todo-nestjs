9ee7571d7b819d137c39dae4e6283fd982d3b015
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-25 10:17:56 +0900

    コントローラーの用意

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { UserService } from './user.service';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from '@prisma/client';
 
+@UseGuards(AuthGuard('jwt')) // 認証が必要なので、全てのエンドポイントに対して認証を行う。JwtStrategyでカスタマイズする
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+}
