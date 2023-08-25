f44a1b69babc9eb1d8ae0acbb6fcf56ed47ac4c7
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-25 10:34:02 +0900

    controllerでエンドポイントを用意

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,4 +9,17 @@ import { User } from '@prisma/client';
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getLoginUser(@Req() req: Request): Omit<User, 'hashedPassword'> {
+    return req.user;
+  }
+
+  @Patch()
+  updateUser(
+    @Req() req: Request,
+    @Body() dto: UpdateUserDto,
+  ): Promise<Omit<User, 'hashedPassword'>> {
+    return this.userService.updateUser(req.user.id, dto);
+  }
 }
